1682e1b8597b2b296d4cdb04c7e7cb6a2f7eeccb
nicolesambile <nicolesambile026@gmail.com>
2026-02-10 18:57:41 -0800

    Add files via upload

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $user = 'root';
 $password = '';
-$database = 'bsis2e';
+$database = 'bsis2b';
 
 $connection = mysqli_connect($host, $user, $password, $database);
 
--- save.php
+++ save.php
@@ -2,16 +2,14 @@
 require('./connection.php');
 
 if(isset($_POST['save'])){
-$fullname = $_POST ['FN'];
-$birthday = $_POST ['BDAY'];
-$address = $_POST ['ADDRESS'];
-$email = $_POST ['EMAIL'];
-$password = $_POST ['PASSWORD'];
+$firstname = $_POST ['FN'];
+$middlename = $_POST ['MN'];
+$lastname = $_POST ['LN'];
 
-$querryCreate = "INSERT INTO bsistbl1 VALUES ('$fullname', '$birthday', '$address', '$email', '$password')";
-$sqlCreate = mysqli_query($connection, $querryCreate);
+$querryCreate = "INSERT INTO bsistbl VALUES (null, '$firstname', '$middlename', '$lastname')";
+$sqlCreate = mysqli_query ($connection, $querryCreate);
 
-echo '<script>alert("Successfully Save!")</script>';
+echo '<script>alert("Successfully Created!")</script>';
 
 }
 
